6f88fe193ff629cbc0a51e981fa5cf9178cfbd04
Priyanty Nurul Fatimah <priyanty.nurul@ui.ac.id>
2021-10-14 14:51:17 +0700

    add new project: sielekthor

---
+++ sielekthor/src/main/java/apap/tugas1/sielekthor/SielekthorApplication.java
@@ -0,0 +1,13 @@
+package apap.tugas1.sielekthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SielekthorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SielekthorApplication.class, args);
+	}
+
+}
